5b76f0c021bc3d65ee297f925146b428d7e1e1f4
Tobbbbbbbbbbbbbbbbbbbbbbbbb <tnelson24@nscds.org>
2020-01-07 15:22:26 +0000

    I reviewed le code and started making random stars

--- Challenges/Sky/Sky.js
+++ Challenges/Sky/Sky.js
@@ -1,4 +1,6 @@
-for (i = 0; i < 1000; i++)
+for (var i = 0; i < 1000; i++)
 {
-    document.body.appendChild(star);
+    var x = Math.floor((Math.random() * 100) + 1);
+    var y = Math.floor((Math.random() * 100) + 1);
+    document.body.appendChild("<div class='star' style='left:x, up:y'></div>");
 }
