7af3bbf47c93c807cff7d36a7a194f979c2878a6
Tobbbbbbbbbbbbbbbbbbbbbbbbb <tnelson24@nscds.org>
2020-01-16 15:22:40 +0000

    worked on shooting star

--- Challenges/Sky/Sky.js
+++ Challenges/Sky/Sky.js
@@ -7,3 +7,5 @@ for (var i = 0; i < 500; i++)
     $("body").prepend("<div class='star' style='left:" + x + "px; top:" + y + "px; height:" + h + "px; width:" + h + "px; opacity:" + o + ";'></div>");
 }
 
+TweenMax.to("#shootingStar",5,{x:1300, y:200, delay:2, repeat:-1});
+
